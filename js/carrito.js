d31209fb5d9fc879f6d0876aacca92168759cebc
lcarballo2022 <107940861+lcarballo2022@users.noreply.github.com>
2022-08-31 17:50:02 -0300

    Add files via upload

--- js/carrito.js
+++ js/carrito.js
@@ -1,10 +1,7 @@
 var myProducts=new Object();
+var products=[];
 
-function add(id, nombreProducto, descripcionProducto){
-  myProducts.id=id;
-  myProducts.nombre=nombreProducto;
-  myProducts.deciption=descripcionProducto;
-  myProducts.foto="https://picsum.photos/80/80" ;
+function add(id, nombreProducto, descripcionProducto, aprice){
     Swal.fire({
         title: 'Esta seguro?',
         text: "Esta seguro que desea agregarlo al carrito!",
@@ -17,12 +14,32 @@ function add(id, nombreProducto, descripcionProducto){
         if (result.isConfirmed) {   
        /*     var addProducts= JSON.parse(localStorage.getItem("product"));
             console.log(addProducts);
-*/
-            localStorage.setItem("product_"+id, JSON.stringify(myProducts));
+*/  
+           // localStorage.setItem("product_"+id, JSON.stringify(myProducts));
+           myProducts.id=id;
+           myProducts.nombre=nombreProducto;
+           myProducts.deciption=descripcionProducto;
+           myProducts.foto="https://picsum.photos/80/80" ;
+           myProducts.price=aprice;
+         
+           products.push(myProducts);
+           document.getElementById("catBuys").innerHTML=products.length;
             Swal.fire("¡Agregado!", "", "success");
           } else{
             Swal.fire("Has cancelado", "", "info");
           }
       })
 }
-
+function show(){
+  let productShow="";
+  let acumulador=0;
+  for (let i = 0; i < products.length; i++) {
+    productShow=productShow+products[i].nombre +"- $"+ products[i].price+"<br>"
+  }
+  let total=products.reduce(
+    (acumulador, p) => acumulador + parseInt(p.price),
+    0
+  );
+  document.getElementById("comprado").innerHTML = productShow + " total:$ "+ total
+  
+}
